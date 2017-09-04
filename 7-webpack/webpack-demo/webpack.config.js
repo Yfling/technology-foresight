40c993dd25ec315ce1a8223d23922bc6a5b680bc
Yfling <598184085@qq.com>
2017-09-04 16:46:21 +0800

    更新2-2webpack配置的entry和out new

--- 7-webpack/webpack-demo/webpack.config.js
+++ 7-webpack/webpack-demo/webpack.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  entry: './src/script/main.js',
+  entry: {
+    main: './src/script/main.js',
+    a: './src/script/a.js'
+  },
   output: {
-    filename:'./dist/js/bundle.js'
+    path: __dirname+'/dist/js',
+    filename: '[name]-[chunkhash].js'      //此处为修改的地方
   }
 }
