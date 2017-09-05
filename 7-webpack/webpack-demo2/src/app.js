73e8e4c6c6d9cee1c68424e2ee19efcb81fde0e7
Yfling <598184085@qq.com>
2017-09-05 11:40:46 +0800

    更新4-2使用babel-loader转换ES6代码(上)

--- 7-webpack/webpack-demo2/src/app.js
+++ 7-webpack/webpack-demo2/src/app.js
@@ -1,7 +1,8 @@
 import layer from './components/layer/layer.js';
 
-const App = function () {
-  console.log(layer);
+const App = function() {
+  const NUM = 1;
+  alert(NUM);
+  console.log(layer)
 }
-
 new App()
--- 7-webpack/webpack-demo2/webpack.config.js
+++ 7-webpack/webpack-demo2/webpack.config.js
@@ -3,8 +3,21 @@ var htmlwebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: './dist',
+    path: __dirname+'/dist/js',
     filename: 'js/[name].bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude:__dirname +'./node_modules/',
+        include:__dirname +'./src/',
+        options: {
+          'presets': ['env']
+        }
+      }
+    ]
   },
   plugins: [
     new htmlwebpackPlugin({
